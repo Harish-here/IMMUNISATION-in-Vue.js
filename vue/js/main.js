8f996a17aeb29d94f98bc474cf4836d6bac0bb96
Harish Ravi <harishamudha@gmail.com>
2017-04-23 08:18:48 -0700

    Dom request

--- vue/js/main.js
+++ vue/js/main.js
@@ -4,22 +4,29 @@ var app = new Vue({
     el: "#myapp",
     data : {
         message : "",
-        datestr : ["45 days","75 Day","105 Days","6 Months","9 Months","10 Months","15 months","18 Months","2 years","5 Years","10 Years"],
+        datestr : ["45 days","75 day","105 days","6 months","9 months","10 months","15 months","18 months","2 years","5 years","10 years"],
         dateres : arr
     },
     methods : {
         blow : function(){
             this.dateres.push("harish");
-        },
-        dotheDate : function(){
-            alert('hi');
         }
     }
+    
 })
 
 $(function(){
 
     $("#datepicker").datepicker();
     $("#datepicker").datepicker("option", "dateFormat","DD, d MM, yy");
+    $("#datepicker").on("change",function(){
+      var now = $(this).val();
+        app.dateres = app.datestr.map(function(val){
+            var num = /\d+/.exec(val); //exetract the number
+            var type = /[dmy]/i.exec(val);// search for the year or day string
+            var newr = moment(app.message).add(num,type).format("D / M / YYYY");
+            return moment(now).add(num,type).format("D / M / YYYY");
+        })
+    })
 
 })
